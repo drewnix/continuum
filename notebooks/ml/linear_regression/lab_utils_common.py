d02e6653ff9e61144f96fc6bfa04add391d5d4d7
Andrew Tanner <andrew@drewnix.dev>
2024-08-26 09:58:49 -0700

    add more nbs

--- notebooks/ml/linear_regression/lab_utils_common.py
+++ notebooks/ml/linear_regression/lab_utils_common.py
@@ -6,7 +6,7 @@ lab_utils_common.py
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.style.use('./deeplearning.mplstyle')
+plt.style.use('deeplearning.mplstyle')
 dlblue = '#0096ff';
 dlorange = '#FF9300';
 dldarkred = '#C00000';
